f3a9c04bf914002b43e08d83f7c98cc94d07ded5
Hazel <99516628+habbleO@users.noreply.github.com>
2026-05-10 20:05:47 +0000

    Finished adding unit tests for Word::right_env_match().

--- phonology-rs/src/test/test_rule.rs
+++ phonology-rs/src/test/test_rule.rs
@@ -169,4 +169,108 @@ fn right_env_match_boundary() {
     assert!(success == true);
 }
 
-// Todo: Finish testing right_env_match.
+#[test]
+fn right_env_match_features() {
+    let cluster_deletion: Rule;
+    let akta = Word::from_str("akta").unwrap();
+
+    {
+        let syllabic = Feature::new("syllabic", Some(true));
+        let min_syllabic = Feature::new("syllabic", Some(false));
+        let input = RuleIO::FeatureMatrix(vec![min_syllabic.clone()]);
+        let output = RuleIO::Nothing;
+
+        let left_env = None;
+        let right_env = Some(vec![Environment::FeatureMatrix(vec![syllabic])]);
+
+        cluster_deletion = Rule::new("Cluster Deletion",
+                                         &input, &output, &left_env, &right_env);
+    }
+
+    let fails = akta.right_env_match(0, &cluster_deletion);
+    assert!(fails == false);
+
+    let fails = akta.right_env_match(1, &cluster_deletion);
+    assert!(fails == false);
+
+    let success = akta.right_env_match(2, &cluster_deletion);
+    assert!(success == true);
+
+    let fails = akta.right_env_match(3, &cluster_deletion);
+    assert!(fails == false);
+
+}
+
+#[test]
+fn right_env_match_segment() {
+    let palatalization: Rule;
+    let sosi = Word::from_str("sosi").unwrap();
+
+    {
+        let s = Segment::from_symbol("s").unwrap();
+        let esh = Segment::from_symbol("ʃ").unwrap();
+
+        let i = Segment::from_symbol("i").unwrap();
+
+        let input = RuleIO::Segment(s);
+        let output = RuleIO::Segment(esh);
+
+        let right_env = Environment::Segment(i);
+
+        palatalization = Rule::new("Palatalization",
+                                   &input, &output, &None, &Some(vec![right_env]));
+    }
+
+    let fails = sosi.right_env_match(0, &palatalization);
+    assert!(fails == false);
+
+    let fails = sosi.right_env_match(1, &palatalization);
+    assert!(fails == false);
+
+    let success = sosi.right_env_match(2, &palatalization);
+    assert!(success == true);
+
+    let fails = sosi.right_env_match(3, &palatalization);
+    assert!(fails == false);
+}
+
+#[test]
+fn right_env_match_multiple() {
+    let akta = Word::from_str("akta").unwrap();
+
+    let new_rule = {
+        let k = Segment::from_symbol("k").unwrap();
+        let t = Segment::from_symbol("t").unwrap();
+        let syllabic = Feature::new("syllabic", Some(true));
+
+        let input = RuleIO::Segment(k);
+        let output = RuleIO::Nothing;
+
+        let left_env = None;
+
+        let r_env_1 = Environment::Segment(t);
+        let r_env_2 = Environment::FeatureMatrix(vec![syllabic]);
+        let r_env_3 = Environment::Boundary;
+
+        let r_env = Some(vec![r_env_1, r_env_2, r_env_3]);
+
+        Rule::new("test", &input, &output, &left_env, &r_env)
+    };
+
+    let fails = akta.right_env_match(0, &new_rule);
+    assert!(fails == false);
+
+    let success = akta.right_env_match(1, &new_rule);
+    assert!(success == true);
+
+    let fails = akta.right_env_match(2, &new_rule);
+    assert!(fails == false);
+
+    let fails = akta.right_env_match(3, &new_rule);
+    assert!(fails == false);
+
+    let aktaa = Word::from_str("aktaa").unwrap();
+
+    let fails = aktaa.right_env_match(1, &new_rule);
+    assert!(fails == false);
+}
